b2b03da8d9a9ec68bde0857cf6106eabc0cc3d08
browlm13 <browlm13@gmail.com>
2017-09-27 22:49:50 -0500

    Update A_WWt_decomp.R
    Force symmetric matrix to be diagonally dominant and then decompose into WtW

--- A_WWt_decomp.R
+++ A_WWt_decomp.R
@@ -10,44 +10,28 @@ symmetric_decomposition <- function(A){
 	return(W)
 }
 
-#can we add elements to diagonal of symetric matrix to ensure it is positive definite?
-#i think the diagnol entries should be greater than each non diagnol entry but im not sure how to prove this
+#Q: Can we add elements to diagonal of a symetric matrix to ensure it is positive definite?
+
+#answer: A diagonally dominant(by rows) symetric matrix with diagonal elements all greater than zero is positive definite.
+
+symmetric_force_positive_definite <- function(A){
+	#force diagonal row dominance on symetric matrix A
+	new_diagonals <- rowSums(abs(A)) + 1
+	diag(A) <- new_diagonals
+	return(A)
+}
+
+#if S is positive definite multiply sqrt(S) by any matrix Q that has orthonormal columns (so that QtQ = I). then Q*sqrt(S) is a choice for A (AtA = S)
+#AtA = (Qsqrt(S))t(Qsqrt(S))= sqrt(S)Qt Qsqrt(S) = S
+
+#positive definite conditions
+#1. All eigenvalues of S must be greater than 0 
+#2. The energy is non negitiv for every: x: xtSx >= 0
+#3.S has the form AtA
+
 if (FALSE){
 dimesions = 15
 A = genPositiveDefMat("c-vine",dim= dimesions)$Sigma
-A = A + 5
-v <- svd(A)$v
-d <- svd(A)$d
-W = v %*% sqrt(diag(d))
-A2 = W %*% t(W)
-
-print(A)
-print(A2)
-print(mse(A,A2))
-
-A = genPositiveDefMat("eigen",dim= dimesions)$Sigma
-A = A + 5
-v <- svd(A)$v
-d <- svd(A)$d
-W = v %*% sqrt(diag(d))
-A2 = W %*% t(W)
-
-print(A)
-print(A2)
-print(mse(A,A2))
-
-A = genPositiveDefMat("onion",dim= dimesions)$Sigma
-A = A + 5
-v <- svd(A)$v
-d <- svd(A)$d
-W = v %*% sqrt(diag(d))
-A2 = W %*% t(W)
-
-print(A)
-print(A2)
-print(mse(A,A2))
-
-A = genPositiveDefMat("unifcorrmat",dim= dimesions)$Sigma
 A = A + 5
 v <- svd(A)$v
 d <- svd(A)$d
