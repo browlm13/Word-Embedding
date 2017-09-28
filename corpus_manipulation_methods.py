8833101e3a663c908dd78c10224cb0ed8d8735d7
browlm13 <browlm13@gmail.com>
2017-09-28 00:03:00 -0500

    Update corpus_manipulation_methods.py
    Method of Symmetric Positive Definite Decomposition Added

--- corpus_manipulation_methods.py
+++ corpus_manipulation_methods.py
@@ -2,6 +2,7 @@
 
 import logging
 from collections import Counter
+import random
 
 #external
 import numpy as np
@@ -95,13 +96,14 @@ def cooccurence_pair_of_distance(sentence_list, d):
 #
 #testing
 #
-corpus = "a b. a b c. a b c d. b c. b c d. c d. d c. d c b. d c b a. a b c d e. e d. e d c."
+corpus = "a b c d. a b. c d."
 unique_words = get_unique_words(corpus)
 n = len(unique_words)
 w2id, id2w = w2id_id2w_maps(unique_words)
 
 #create empty cooccurence matrix
-A = np.zeros([n,n],np.float32)
+#A = np.zeros([n,n],np.float32)
+A = np.ones([n,n],np.float32)
 
 #compute cooccurence matrix
 sentences = sent_tokenize(corpus)
@@ -119,14 +121,11 @@ for s in sentences:
 			A[p[1],p[0]] += ngram_inc_amt(d)
 
 #finished cooccurence matrix A
-print (w2id)
-print (A)
 
 # intilize random word vector (range (-0.5, 0.5])
 vector_size = n #not sure how to choose thise
 #W = ((np.random.rand(vector_size, n) - 0.5) / float(vector_size + 1))
 W = ((np.random.rand(vector_size, n) - 2) / float(vector_size + 1))
-print (W)
 
 def total_squared_error():
 	total_error = 0
@@ -137,6 +136,9 @@ def total_squared_error():
 				wj = W[:,j]
 				total_error += (np.dot(wi, wj) - A[i,j])**2
 	return total_error
+
+def mean_squared_error(A, B):
+	return ((A - B) ** 2).mean(axis=None)
 
 def djdx(x):
 	wi = np.transpose(W[:,x])
@@ -156,14 +158,27 @@ def dot_improvement():
 				print ("A[%d,%d] = %f" % (i,j,A[i,j]))
 				print ("w[%d] dot w[%d] = %f" % (i,j, np.dot(wi, wj)))
 
-import random
+#
+#	Cooccurence Matrix
+#
+
+print ("Word Map:\n")
+print (w2id)
+print("\nCooccurence Matrix:\n")
+print (A)
+
+#
+# Stocastic Gradient Decent Decomposition
+#
 
 iterations = 10000
-learning_rate = 0.0001
+learning_rate = 0.001
 starting_error = total_squared_error()
 
-dot_improvement()
-print ("starting total squared error: %f" % total_squared_error())
+print("\n\n\nStocastic Gradient Decent Decomposition Training:\n")
+
+#dot_improvement()
+print ("starting mean squared error: %f" % mean_squared_error(A, W))
 
 for i in range(iterations):
 
@@ -172,17 +187,91 @@ for i in range(iterations):
 
 
 print("\n\n\nFinished training\n\n\n")
-dot_improvement()
+#dot_improvement()
 ending_error = total_squared_error()
-print ("final total squared error: %f" % total_squared_error())
+print ("\nfinal mean squared error: %f" % mean_squared_error(A, W))
 print ("error improvment = %f" % (starting_error - ending_error))
 
 
-"""
-#print(ngram_tupples(corpus,2))
+print("\n\n\nStocastic Gradient Decent Decomposition Results:\n")
 
-sent_tokens = ['1','1','2','3', '4', '1']
-l = [t for t in ngrams(sent_tokens,2)]
-print (l)
+print("\n\nA:")
+print (A)
+print("\nWtW:\n")
+print(np.dot(W, np.transpose(W)))
+
+print("\nMean Squared Error: %f\n" % mean_squared_error(W,A))
+
+
+"""
+Step 1. Force Cooccurence Matrix A to Positive Definite Matrix
+	" A diagonally dominant(by rows) symetric matrix with diagonal elements all greater than zero is positive definite."
+	-A is already symetric 
+	-Make diagonally dominant with diagnonal entries greater than 0
+
+Step 2. Symmetric Positive Definite Matrix Decomposition
+	" If A is positive definite, then A = WtW "
+	-SVD: A=UDV
+	-W = Vsqrt(diagnol(D))
+
+code:
+	# take absolute value of A
+	A = np.absolute(A)
+
+	# Sum rows in a
+	new_diagonals = A.sum(axis=1)
+
+	#replace diagnols in A
+	np.fill_diagonal(A, new_diagonals)
+
+	# singular value decomposition
+	U, D, V = np.linalg.svd(A, full_matrices=False)
+
+	# Create matrix W = Vtsqrt(diagnol(D)) #why Vt?
+	W = np.dot(np.transpose(V), np.sqrt(np.diag(D)))
+
+	#A = WWt
 """
 
+def force_symmetric_to_symmetric_positive_definite(S):
+	""" Take absolute value of S and update diagnol entries to make a diagonally dominant matrix with diagonal entries greater than 0. """
+	# take absolute value of S
+	S = np.absolute(S)
+
+	# Sum rows in S
+	new_diagonals = S.sum(axis=1)
+
+	#replace diagnols in S
+	np.fill_diagonal(S, new_diagonals)
+
+	return S
+
+def symmetric_positive_definite_decomposition(P):
+	""" return M such that P = MMt, where matrix parameter P is SPD """
+	# singular value decomposition
+	U, D, V = np.linalg.svd(P, full_matrices=False)
+
+	# Create matrix W = Vtsqrt(diagnol(D)) #why Vt?
+	M = np.dot(np.transpose(V), np.sqrt(np.diag(D)))
+
+	return M
+
+def spd_decomposition(S):
+	""" Force Cooccurence Matrix A to Positive Definite Matrix and decompose into W such that A = WWt. """
+	P = force_symmetric_to_symmetric_positive_definite(S)
+	M = symmetric_positive_definite_decomposition(P)
+	return M
+
+#
+# Perform Symmetric Positive Definite Decomposition
+#
+
+W = spd_decomposition(A)
+
+print("\n\n\nSymmetric Positive Definite Decomposition:\n")
+print("\n\nA:")
+print (A)
+print("\n\nA2 (modified into SPD):\n")
+print(force_symmetric_to_symmetric_positive_definite(A))
+print("\nWWt:\n")
+print(np.dot(W, np.transpose(W)))
