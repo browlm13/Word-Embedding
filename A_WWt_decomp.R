3e462bde1eae627cd212942e723478c4a10f18e1
browlm13 <browlm13@gmail.com>
2017-09-27 20:27:29 -0500

    Update A_WWt_decomp.R
    for symmetric positive definite matrices -> A = WWt if W = v sqrt(d), where A = udv (using singular value decomposition)

--- A_WWt_decomp.R
+++ A_WWt_decomp.R
@@ -1,6 +1,18 @@
 library(clusterGeneration)
 library(Metrics)
 
+#works for symetric positive definite matrices (eigen values must be positive)
+symmetric_decomposition <- function(A){
+
+	v <- svd(A)$v
+	d <- svd(A)$d
+	W = v %*% sqrt(diag(d))
+	return(W)
+}
+
+#can we add elements to diagonal of symetric matrix to ensure it is positive definite?
+#i think the diagnol entries should be greater than each non diagnol entry but im not sure how to prove this
+if (FALSE){
 dimesions = 15
 A = genPositiveDefMat("c-vine",dim= dimesions)$Sigma
 A = A + 5
@@ -46,3 +58,4 @@ print(A)
 print(A2)
 print(mse(A,A2))
 print(mae(A,A2))
+}
