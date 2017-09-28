e7650b17397a6ec8f45684f642f69cd009ded490
browlm13 <browlm13@gmail.com>
2017-09-28 12:35:24 -0500

    Update A_WWt_decomp.R

--- A_WWt_decomp.R
+++ A_WWt_decomp.R
@@ -2,12 +2,13 @@ library(clusterGeneration)
 library(Metrics)
 
 #works for symetric positive definite matrices (eigen values must be positive)
-symmetric_decomposition <- function(A){
-
-	v <- svd(A)$v
+symmetric_positive_definite_decomposition <- function(A){
+   v <- svd(A)$v
 	d <- svd(A)$d
-	W = v %*% sqrt(diag(d))
-	return(W)
+	W = v %*% sqrt(diag(d))	#WWt
+	#W = sqrt(diag(d)) %*% t(v)	#WtW
+	#u = v
+   return(W)
 }
 
 #Q: Can we add elements to diagonal of a symetric matrix to ensure it is positive definite?
@@ -29,6 +30,15 @@ symmetric_force_positive_definite <- function(A){
 #2. The energy is non negitiv for every: x: xtSx >= 0
 #3.S has the form AtA
 
+#symmetric positive definite decomposition with spectral decomposition/ EVD
+spd_decomposition <- function(A){
+	#if AtA == AAt then, 
+	q <- matrix(eigen(A %*% t(A))$vectors, nrow=nrow(A))
+	d = c(eigen(A)$values)
+	W = q %*% sqrt(diag(d))
+   return(W)
+}
+
 if (FALSE){
 dimesions = 15
 A = genPositiveDefMat("c-vine",dim= dimesions)$Sigma
@@ -42,4 +52,29 @@ print(A)
 print(A2)
 print(mse(A,A2))
 print(mae(A,A2))
+}
+
+if (FALSE){
+	#if A is semetric AtA = AAt
+	AtA <- t(A) %*% A
+	AAt <- A %*% t(A)
+	
+	#why do the eigen vectors have diffrent signs???
+	#its because they are not identical according to the computer
+	#then if they are identical... (AtA == AAt)
+	#d = c(eigen(A)$values)
+	#A == AtA %*% diag(d) %*% t(AtA)
+	#A == AAt %*% diag(d) %*% t(AAt)
+	
+	#v = matrix(eigen(AAt)$vectors, nrow=nrow(AAt))
+	#W = AAt %*% sqrt(diag(d))
+	#A == W %*% t(W)
+	
+	#tested --
+	AAt <- matrix(eigen(A %*% t(A))$vectors, nrow=nrow(A))
+	d = c(eigen(A)$values)
+	#then the next line is equal to A
+	AAt %*% diag(d) %*% t(AAt)
+	W2 = AAt %*% sqrt(diag(d))
+	W2 %*% t(W2)
 }
